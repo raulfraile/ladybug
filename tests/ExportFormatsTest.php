9471a8b2c6c8c50822fbb984163606d781685751
Raul Fraile <raulfraile@gmail.com>
2012-01-24 10:57:42 +0100

    [tests] Added test conditions

--- tests/ExportFormatsTest.php
+++ tests/ExportFormatsTest.php
@@ -21,20 +21,20 @@ class ExportFormatsTest extends PHPUnit_Framework_TestCase
     }
     
     public function testExportYamlFormat() {
-        $data = ladybug_dump_return('yaml', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5]);
+        $data = ladybug_dump_return('yaml', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5], $this->vars[6]);
         
         $this->assertEquals(file_get_contents(__DIR__ . '/files/test.yml'), $data);
     }
     
     public function testExportXmlFormat() {
-        $data = ladybug_dump_return('xml', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5]);
+        $data = ladybug_dump_return('xml', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5], $this->vars[6]);
         
         $this->assertEquals(file_get_contents(__DIR__ . '/files/test.xml'), $data);
     }   
     
     public function testExportJsonFormat() {
-        $data = ladybug_dump_return('json', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5]);
-
+        $data = ladybug_dump_return('json', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5], $this->vars[6]);
+        
         $this->assertEquals(file_get_contents(__DIR__ . '/files/test.json'), $data);
     }
     
@@ -42,7 +42,7 @@ class ExportFormatsTest extends PHPUnit_Framework_TestCase
      * @expectedException Ladybug\Exception\InvalidFormatException
      */
     public function testUnknownFormatThrowsException() {
-        $data = ladybug_dump_return('unknown', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5]);
+        $data = ladybug_dump_return('unknown', $this->vars[0], $this->vars[1], $this->vars[2], $this->vars[3], $this->vars[4], $this->vars[5], $this->vars[6]);
         $this->assertEquals(file_get_contents(__DIR__ . '/files/test.unknown'), $data);
     }   
 }
