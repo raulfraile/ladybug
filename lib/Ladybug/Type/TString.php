9d03eab167c1b9f84f9a832c2e26140a780f6100
Raul Fraile <raulfraile@gmail.com>
2012-07-12 18:19:19 +0200

    Fixed problem with encodings not available in htmlentities function

--- lib/Ladybug/Type/TString.php
+++ lib/Ladybug/Type/TString.php
@@ -48,7 +48,13 @@ class TString extends TBase
 
     protected function _renderHTML($array_key = null)
     {
-        return '<div class="final">'.$this->renderArrayKey($array_key).'<span class="type">'.$this->type.'('.$this->length.')</span> <span style="color:'.$this->getColor('html').'">'.htmlentities($this->getValue(), ENT_COMPAT, 'UTF-8').'</span></div>';
+        return '<div class="final">'.
+            $this->renderArrayKey($array_key).
+            '<span class="type">' .
+                $this->type . '(' . $this->length . ')</span> ' .
+                '<span style="color:' . $this->getColor('html') . '">' .
+                    htmlentities($this->getValue(), ENT_COMPAT, $this->_getEncodingForHtmlentities()) .
+                '</span></div>';
     }
 
     protected function _renderCLI($array_key = null)
@@ -69,5 +75,31 @@ class TString extends TBase
             'length' => $this->length,
             'encoding' => $this->encoding
         );
+    }
+
+    private function _getEncodingForHtmlentities()
+    {
+        $validEncodings = array(
+            'iso-8859-1', 'iso8859-1',
+            'iso-8859-5', 'iso8859-5',
+            'iso-8859-15', 'iso8859-15',
+            'utf-8',
+            'cp866', 'ibm866', '866',
+            'cp1251', 'Windows-1251', 'win-1251', '1251',
+            'cp1252', 'Windows-1252', '1252',
+            'KOI8-R', 'koi8-ru', 'koi8r',
+            'BIG5', '950',
+            'GB2312', '936',
+            'BIG5-HKSCS',
+            'Shift_JIS', 'SJIS', 'SJIS-win', 'cp932', '932',
+            'EUC-JP', 'EUCJP', 'eucJP-win',
+            'MacRoman'
+        );
+
+        if (in_array($this->encoding, $validEncodings)) {
+            return $this->encoding;
+        } else {
+            return 'ISO-8859-1';
+        }
     }
 }
