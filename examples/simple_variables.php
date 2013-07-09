d9c11e72b10e871ed094c6a6361dc547ccde84a4
Raul Fraile <raulfraile@gmail.com>
2013-07-10 01:38:16 +0200

    Composer cleanups

--- examples/simple_variables.php
+++ examples/simple_variables.php
@@ -11,7 +11,7 @@ $var5 = false;
 $var6 = 'Hello world!';
 
 $dumper = new \Ladybug\Dumper();
-$dumper->setTheme('base');
-$dumper->setFormat(\Ladybug\Format\XmlFormat::FORMAT_NAME);
+//$dumper->setTheme('base');
+//$dumper->setFormat(\Ladybug\Format\XmlFormat::FORMAT_NAME);
 
 echo $dumper->dump($var1, $var2, $var3, $var4, $var5);
