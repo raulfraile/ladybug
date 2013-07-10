82974bca31fc329f3201dce162ef4a7712ed9e32
Raul Fraile <raulfraile@gmail.com>
2013-07-10 16:34:52 +0200

    Added caller info to base theme

--- examples/simple_variables.php
+++ examples/simple_variables.php
@@ -11,7 +11,7 @@ $var5 = false;
 $var6 = 'Hello world!';
 
 $dumper = new \Ladybug\Dumper();
-//$dumper->setTheme('base');
+$dumper->setTheme('modern');
 //$dumper->setFormat(\Ladybug\Format\XmlFormat::FORMAT_NAME);
 
 echo $dumper->dump($var1, $var2, $var3, $var4, $var5);
