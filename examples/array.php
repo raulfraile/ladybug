852846694a4fd4b54147706fdb984206799c4c66
Raul Fraile <raulfraile@gmail.com>
2013-07-12 18:24:51 +0200

    Updated readme file

--- examples/array.php
+++ examples/array.php
@@ -3,31 +3,15 @@
 require_once __DIR__.'/../vendor/autoload.php';
 Ladybug\Loader::loadHelpers();
 
-$var1 = array();
-
-$var1[0] = array(
-    'name' => 'Raul',
-    'url' => 'http://twitter.com/raulfraile',
-    'stats' => array(
-        array(1,2,3),
-        array(3,4)
+$var = array(
+    array(
+        'name' => 'Raul',
+        'age' => 29
+    ),
+    array(
+        'name' => 'John',
+        'age' => 27
     )
 );
 
-$var1[1] = array(
-    'name' => 'John',
-    'age' => 27
-);
-
-$var1[2] = array(
-    '<<name>>' => '<<Ladybug>>',
-    '<<age>>' => 2
-);
-
-//$var1[3] = new stdClass();
-//$var1[4] = fopen(__DIR__.'/images/ladybug.png', 'r');
-
-$dumper = new \Ladybug\Dumper();
-//$dumper->setTheme('base');
-//$dumper->setFormat(\Ladybug\Format\HtmlFormat::FORMAT_NAME);
-echo $dumper->dump($var1);
+ladybug_dump($var);
--- examples/simple_variables.php
+++ examples/simple_variables.php
@@ -3,22 +3,10 @@
 require_once __DIR__.'/../vendor/autoload.php';
 Ladybug\Loader::loadHelpers();
 
-$var1 = null;
+$var1 = NULL;
 $var2 = 15;
 $var3 = 15.5;
-$var4 = 'Россия';// 'hello world!';
+$var4 = 'hello world!';
 $var5 = false;
-$var6 = 'Hello world!';
 
-$dumper = new \Ladybug\Dumper();
-$dumper->setTheme('base');
-//$dumper->setFormat(\Ladybug\Format\XmlFormat::FORMAT_NAME);
-
-
-ladybug_set_theme('base');
-ldd($var2);
-
-echo $dumper->dump($var1, $var2, $var3, $var4, $var5);
-
-
-//ldd($var1);
+ladybug_dump($var1, $var2, $var3, $var4, $var5);
