4a2bfc0abad7caa433d13b88c387f02d022771c5
Raul Fraile <raulfraile@gmail.com>
2013-07-16 18:47:03 +0200

    Fixed array template for text format

--- examples/simple_variables.php
+++ examples/simple_variables.php
@@ -8,4 +8,5 @@ $var3 = 15.5;
 $var4 = 'hello world!';
 $var5 = false;
 
-ladybug_dump($var1, $var2, $var3, $var4, $var5);
+ladybug_set_format('text');
+ladybug_dump($var1, $var2, $var3, $var4, $var5, array(1,2,3));
