ad495b552540e009a09e39b70f25ed7017c2e9f9
Raul Fraile <raulfraile@gmail.com>
2013-08-13 23:07:34 +0200

    Fixed max nesting level issues

--- examples/array.php
+++ examples/array.php
@@ -5,11 +5,18 @@ require_once __DIR__.'/../vendor/autoload.php';
 $var = array(
     array(
         'name' => 'Raul',
-        'age' => 29
+        'age' => 29,
+        'languages' => array(
+            'Spanish',
+            'English'
+        ),array(array(array(array(array(array(array(array(array(1,2,3)))))))))
     ),
     array(
         'name' => 'John',
-        'age' => 27
+        'age' => 27,
+        'languages' => array(
+            'English'
+        )
     )
 );
 
