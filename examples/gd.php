85f81ad12676863ea3ce74d850c4f408a5cbfd67
Raul Fraile <raulfraile@gmail.com>
2013-08-14 18:24:17 +0200

    Refactored modern theme

--- examples/gd.php
+++ examples/gd.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 Ladybug\Loader::loadHelpers();
 
 $img = imagecreatefrompng(__DIR__ . '/images/ladybug.png');
-$img = imagecreatefrompng('/Users/raulfraile/Sites/servergrove/sgcontrol2/web/images/nav_02.png');
+
 $dumper = new \Ladybug\Dumper();
 
 echo $dumper->dump($img);
